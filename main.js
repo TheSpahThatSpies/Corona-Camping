c89e1d1008b5211b65ded5a4e01f01965ab3117a
TheSpahThatSpies <christianborggaard@gmail.com>
2021-06-01 13:54:14 +0200

    Fortsætning af forside

--- main.js
+++ main.js
@@ -17,26 +17,3 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 4000);
   }
-
-  // Om Corona Camping Trustpilot reviews
-  var reviewsIndex = 1;
-  showReviews(reviewsIndex);
-
-  function plusReviews(n) {
-      showReviews(reviewsIndex += n);
-  }
-
-  function currentReview(n) {
-      showReviews(reviewsIndex = n);
-  }
-
-  function showReviews(n) {
-    var i;
-    var reviews = document.getElementsByClassName("trustpilot-slide");
-    if (n > reviews.length) {reviewIndex = 1}
-    if (n < 1) {reviewsIndex = reviews.length}
-    for (i = 0; i < reviews.length; i++) {
-        reviews[i].style.display = "none";
-    }
-    reviews[reviewsIndex-1].style.display = "block";
-  }
