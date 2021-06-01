9dff445cff92f88a6e3d3023b7cb9e5df6c8c1f1
TheSpahThatSpies <christianborggaard@gmail.com>
2021-06-01 13:01:37 +0200

    trustpilot setup på om corona camping

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Forside slideshow
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -15,4 +16,27 @@ function showSlides() {
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 4000);
+  }
+
+  // Om Corona Camping Trustpilot reviews
+  var reviewsIndex = 1;
+  showReviews(reviewsIndex);
+
+  function plusReviews(n) {
+      showReviews(reviewsIndex += n);
+  }
+
+  function currentReview(n) {
+      showReviews(reviewsIndex = n);
+  }
+
+  function showReviews(n) {
+    var i;
+    var reviews = document.getElementsByClassName("trustpilot-slide");
+    if (n > reviews.length) {reviewIndex = 1}
+    if (n < 1) {reviewsIndex = reviews.length}
+    for (i = 0; i < reviews.length; i++) {
+        reviews[i].style.display = "none";
+    }
+    reviews[reviewsIndex-1].style.display = "block";
   }
